02f1379ceb201d7f0e955a364a8a0a1537f7ef83
vnln <bananiqus@gmail.com>
2022-01-03 00:25:53 +0400

    fix: add comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 module.exports = (n) => [...(Math.abs(n) + "")].reverse().join("");
+
+// 1. getting rid of negative sign with Math.abs
+// 2. adding empty string to convert to string type
+// 3. unpacking with spread operator
+// 4. reversing and joining together to achieve desired results
